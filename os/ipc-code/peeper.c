7450a99c1daa283d41cd763942b43afc28653e5a
hotramen-hellfire <22b0956@iitb.ac.in>
2024-02-15 15:36:06 +0530

    completed piper, peepeer. . .

---
+++ os/ipc-code/peeper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+char *ptr = "hola";
+char *pathname = "/tmp/aloha";
+
+int main()
+{
+    int fd = open(pathname, O_RDONLY);
+    char messagerecv[45];
+    read(fd, messagerecv, sizeof(messagerecv));
+    printf(messagerecv);
+    // write(fd, message, strlen(message) + 1);
+    close(fd);
+    return 0;
+}
