4aa80f52d3b2dfe8ace40b3dee92c166061869f3
hotramen-hellfire <himanshu16gangwal@iitb.ac.in>
2024-05-22 14:49:25 +0530

    add philo

--- os/weaving/master-worker/worker_thread_id = (int *)malloc(sizeof(.c
+++
@@ -1,7 +0,0 @@
-   worker_thread_id = (int *)malloc(sizeof(int) * num_workers);
-   worker_thread = (pthread_t *)malloc(sizeof(pthread_t) * num_workers);
-  for (i = 0; i < num_workers; i++)
-    worker_thread_id[i] = i;
-
-  for (i = 0; i < num_workers; i++)
-    pthread_create(&worker_thread[i], NULL, generate_requests_loop, (void *)&worker_thread_id[i]);
